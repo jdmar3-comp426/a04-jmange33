93d1a58508557f20bbd3eb25d6eebd4c96bac7a6
Jake Mangeney <jakemangeney@gmail.com>
2021-11-30 14:36:02 -0500

    more message tweaking

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.get("/app/", (req, res, next) => {
 
 app.patch("/app/", (req, res, next) => {
     res.json({"message":"1 record updated: id 2 (200)"});
-	res.status(200);
+	res.status(201);
 });
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
@@ -43,7 +43,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id = ?").all();
-	res.status(200).json(stmt);
+	res.status(201).json(stmt);
 })
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
