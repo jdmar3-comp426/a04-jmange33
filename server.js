5fd2ae0a22d8a57ce7f3c3436661ac46bb038688
Jake Mangeney <jakemangeney@gmail.com>
2021-11-30 15:38:28 -0500

    confusion

--- server.js
+++ server.js
@@ -41,12 +41,12 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/user/:id", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = :id").all();
+	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = 2").all();
 	res.status(200).json(stmt);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id = ?");
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id = 2");
 	stmt.run("oldtest", "pass");
 	res.json({"message":"1 record updated: id 2 (200)"});
 });
