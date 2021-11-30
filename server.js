db336a246e643bf688e095249b11f63df5de294a
Jake Mangeney <jakemangeney@gmail.com>
2021-11-30 14:50:03 -0500

    run instead of prepare

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/app/user/:id", (req, res) => {
 })
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id = ?").all();
+	const stmt = db.run("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id = ?").all();
 	res.json({"message":"1 record updated: id 2 (200)"});
 })
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
