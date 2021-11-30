1b865b1d9a3419b6d411da0ad437698aca23e586
Jake Mangeney <jakemangeney@gmail.com>
2021-11-30 16:22:36 -0500

    idk no mo

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ app.get("/app/user/:id", (req, res) => {
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id = 2");
-	stmt.run("oldtest", "9b1bb58afcc8132637f891cd1b6360ad");
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(oldtest, user), pass = COALESCE(adifferentpassword,pass) WHERE id = 2");
+	stmt.run();
 	res.json({"message":"1 record updated: ID 2 (200)"});
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
