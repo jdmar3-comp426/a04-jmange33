f89ea79d2afa6a5caf14e97dd7741cbd4a2a2c91
Jake Mangeney <jakemangeney@gmail.com>
2021-11-30 14:43:30 -0500

    no more status

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?,pass) WHERE id = ?").all();
-	res.status(200).json({"message":"1 record updated: id 2 (200)"});
+	res.json({"message":"1 record updated: id 2 (200)"});
 })
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
